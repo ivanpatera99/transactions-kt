f978c2b38460f35cb0b965af8d1ce269546d7a54
ipatera <ivanpatera99@gmail.com>
2024-06-05 18:58:46 -0300

    feat(transaction): define interface for service

---
+++ src/main/kotlin/com/ivanpatera/mendel/services/ITransactionService.kt
@@ -0,0 +1,5 @@
+interface ITransactionService {
+    fun putTransaction(transactionId: Long, amount: Double, type: String, parentId: Long)
+    fun getTransactionsForType(type: String)
+    fun getTransactionsSum(transactionId: Long): Double
+}
